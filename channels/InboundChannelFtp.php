a014c2d77fd1360a30355f4199d160ac1dfaa561
Marçal Pla <mpla@emailingnetwork.com>
2015-10-23 20:37:45 +0200

    Improvements.

--- channels/InboundChannelFtp.php
+++ channels/InboundChannelFtp.php
@@ -1,43 +1,43 @@
 <?php
-class InboundChannelFtp {
+class InboundChannelFtp
+{
   private $user;
   private $password;
   private $host;
   private $path;
+  private $prefix;
 
   private $localAuxPath;
   private $localAuxFileTracker;
 
-  private function getFtpMode($file)
+  private $waitLoop = 5;
+
+  public function __construct($user, $password, $host, $path, $prefix = NULL)
   {
-    $path_parts = pathinfo($file);
-
-    if (!isset($path_parts['extension'])) return FTP_BINARY;
-
-    switch (strtolower($path_parts['extension'])) {
-      case 'am':case 'asp':case 'bat':case 'c':case 'cfm':case 'cgi':case 'conf':
-      case 'cpp':case 'css':case 'csv':case 'dhtml':case 'diz':case 'h':case 'hpp':case 'htm':
-      case 'html':case 'in':case 'inc':case 'js':case 'log':case 'm4':case 'mak':case 'nfs':
-      case 'nsi':case 'pas':case 'patch':case 'php':case 'php3':case 'php4':case 'php5':
-      case 'phtml':case 'pl':case 'po':case 'py':case 'qmail':case 'sh':case 'shtml':
-      case 'sql':case 'tcl':case 'tpl':case 'txt':case 'vbs':case 'xml':case 'xrc':
-        return FTP_ASCII;
-    }
-
-    return FTP_BINARY;
-  }
-
-  public function __construct($user, $password, $host, $path) {
     $this->user = $user;
     $this->password = $password;
     $this->host = $host;
     $this->path = $path;
+    $this->prefix = isset($prefix) ? explode(",", $prefix) : NULL;
 
     $this->localAuxPath = ROOT_PATH . DIRECTORY_SEPARATOR . '.' . $user . $host . str_replace('\\', '_', str_replace('/', '-', $path));
     $this->localAuxFileTracker = $this->localAuxPath . DIRECTORY_SEPARATOR . 'fileTracker.log';
   }
 
-  public function getFiles() {
+  private function matchFile($file) {
+    $match = false;
+    if(isset($this->prefix)) {
+      foreach($this->prefix as $prefix) {
+        if(!$match) $match = substr($file, 0, strlen($prefix)) === $prefix;
+      }
+    } else {
+      $match = true;
+    }
+    return $match;
+  }
+
+  public function getFiles()
+  {
     $remoteFiles = array();
     $localFiles = array();
 
@@ -50,14 +50,16 @@ class InboundChannelFtp {
 
     if(!($ftpStream = ftp_connect($this->host))) throw new Exception('No se ha podido conectar a ' + $this->host + '.');
     if(!ftp_login($ftpStream, $this->user, $this->password)) throw new Exception('No se ha podido hacer login.');
-    if(!ftp_chdir($ftpStream,$this->$path)) throw new Exception('No se ha podido acceder al directorio remoto ' + $this->path);
+    if(!ftp_chdir($ftpStream, $this->path)) throw new Exception('No se ha podido acceder al directorio remoto ' + $this->path);
     $remoteFiles = ftp_nlist($ftpStream, '.');
 
     foreach($remoteFiles as $remoteFile) {
-      if(strpos(file_get_contents($this->localAuxFileTracker),$remoteFile) === false) {
+      if($this->matchFile($remoteFile) && strpos(file_get_contents($this->localAuxFileTracker),$remoteFile) === false) {
         $localFile = $this->localAuxPath . DIRECTORY_SEPARATOR . $remoteFile;
 
-        if(ftp_get($ftpStream, $localFile, $remoteFile, getFtpMode($remoteFile))) {
+        sleep($this->waitLoop);
+        
+        if(ftp_get($ftpStream, $localFile, $remoteFile, FTP_BINARY)) {
           $localFiles[] = $localFile;
           file_put_contents($this->localAuxFileTracker, $remoteFile . ';', FILE_APPEND | LOCK_EX);
         }
--- channels/OutboundChannelS3.php
+++ channels/OutboundChannelS3.php
@@ -4,36 +4,46 @@ require ROOT_PATH . DIRECTORY_SEPARATOR . 'vendor' . DIRECTORY_SEPARATOR . 'aws'
 use Aws\S3\S3Client;
 use Aws\S3\Exception\S3Exception;
 
-class OutboundChannelS3 {
+class OutboundChannelS3
+{
   private $s3;
 
   private $bucket;
   private $path;
 
-  public function __construct($key, $secret, $bucket, $path) {
-    $this->s3 = S3Client::factory(
+  private $waitLoop = 5;
+
+  public function __construct($key, $secret, $region, $bucket, $path)
+  {
+    $this->s3 = S3Client::factory(array(
       'credentials' => array(
         'key' => $key,
         'secret' => $secret
-      )
-    );
+      ),
+      'version' => '2006-03-01',
+      'region' => $region
+    ));
 
     $this->bucket = $bucket;
     $this->path = rtrim($path, '/') . '/';
   }
 
-  public function putFile($file, $s3StorageClass = 'STANDARD', $s3ACL = 'private') {
+  public function putFile($file, $s3StorageClass = 'STANDARD', $s3ACL = 'private')
+  {
     $finfo = finfo_open(FILEINFO_MIME_TYPE);
-    $result = $s3->putObject(array(
-        'Bucket'       => $this->bucket,
-        'Key'          => $this->path . basename($file),
-        'SourceFile'   => $file,
-        'ContentType'  => finfo_file($finfo, $file),
-        'ACL'          => $s3ACL,
-        'StorageClass' => $s3StorageClass
+
+    sleep($this->waitLoop);
+    
+    $result = $this->s3->putObject(array(
+      'Bucket'       => $this->bucket,
+      'Key'          => $this->path . basename($file),
+      'SourceFile'   => $file,
+      'ContentType'  => finfo_file($finfo, $file),
+      'StorageClass' => $s3StorageClass,
+      'ACL'          => $s3ACL
     ));
     finfo_close($finfo);
-    unlink($file);
+    unlink($file); var_dump($result);
   }
 }
 ?>
