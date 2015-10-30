297989cd6a54c3631a70647613cd770388e8ee6f
Marçal Pla <mpla@emailingnetwork.com>
2015-10-30 11:52:27 +0100

    Improvement.

--- launcher_example.sh
+++ launcher_example.sh
@@ -15,7 +15,8 @@ OUTBOUND_REGION=value
 OUTBOUND_BUCKET=value
 OUTBOUND_PATH=value
 
+LOG_PATH=value
+
 DATAPIPE_PATH=value
 
-/usr/bin/php $DATAPIPE_PATH --inbound-channel=$INBOUND_CHANNEL --inbound-user=$INBOUND_USER --inbound-password=$INBOUND_PASSWORD --inbound-host=$INBOUND_HOST --inbound-path=$INBOUND_PATH --inbound-prefix=$INBOUND_PREFIX
---outbound-channel=$OUTBOUND_CHANNEL --outbound-key=$OUTBOUND_KEY --outbound-secret=$OUTBOUND_SECRET --outbound-region=$OUTBOUND_REGION --outbound-bucket=$OUTBOUND_BUCKET --outbound-path=$OUTBOUND_PATH
+/usr/bin/php $DATAPIPE_PATH --inbound-channel=$INBOUND_CHANNEL --inbound-user=$INBOUND_USER --inbound-password=$INBOUND_PASSWORD --inbound-host=$INBOUND_HOST --inbound-path=$INBOUND_PATH --inbound-prefix=$INBOUND_PREFIX --outbound-channel=$OUTBOUND_CHANNEL --outbound-key=$OUTBOUND_KEY --outbound-secret=$OUTBOUND_SECRET --outbound-region=$OUTBOUND_REGION --outbound-bucket=$OUTBOUND_BUCKET --outbound-path=$OUTBOUND_PATH >> $LOG_PATH 2>&1
